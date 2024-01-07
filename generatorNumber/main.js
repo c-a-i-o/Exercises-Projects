fcc64e2597da5c87bd6e0e3ed777b7d6145da8f1
Caio Borges <caiovbs@gmail.com>
2024-01-07 12:24:50 +0000

    fixAdd

--- generatorNumber/main.js
+++ generatorNumber/main.js
@@ -6,7 +6,7 @@ let generateOTP = () => {
 
     const otpLength = 6;
 
-    const otp = Math.floor(100000 + Math.random() * 900000);
+    const otp = Math.floor(1000 + Math.random() * 9000);
 
     document.getElementById("otpDisplay").innerText = `${otp}`;
 
